671992ac5cebdbc2fe0e7d65de0347e700cc97ea
liming.wang <liming.wang@zkjin.com>
2018-11-09 22:46:50 +0800

    add graphql demo

---
+++ graphql-demo/src/main/java/com/leon/demo/graphql/HelloWorld.java
@@ -0,0 +1,37 @@
+package com.leon.demo.graphql;
+
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+/**
+ * @Author : leonwang
+ * @Descpriction
+ * @Date:created 2018/11/8
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        String schema = "type Query{hello: String}";
+
+        SchemaParser schemaParser = new SchemaParser();
+        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+
+        RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
+                .type("Query", builder -> builder.dataFetcher("hello", new StaticDataFetcher("world")))
+                .build();
+
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+
+        GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
+        ExecutionResult executionResult = build.execute("{hello}");
+
+        System.out.println(executionResult.getData().toString());
+        // Prints: {hello=world}
+    }
+}
